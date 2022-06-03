e88e377b1a802aa95cc2b9f2c9dcd5ca4cbd371d
ytaousi <yassir.taous@gmail.com>
2022-06-03 23:58:37 +0100

    pull request test

--- src/CGI/CGI.hpp
+++ src/CGI/CGI.hpp
@@ -30,7 +30,21 @@ namespace	ws
 					return 1; // file not found?
 
 				// setup the environment variables
-
+				envp.insert(std::make_pair<std::string, std::string>("SERVER_NAME", "should be the server name"));
+				envp.insert(std::make_pair<std::string, std::string>("SERVER_SOFTWARE", "should be the server software"));
+				envp.insert(std::make_pair<std::string, std::string>("GATEWAY_INTERFACE", "CGI/1.1"));
+				envp.insert(std::make_pair<std::string, std::string>("SERVER_PROTOCOL", "HTTP/1.1"));
+				envp.insert(std::make_pair<std::string, std::string>("SERVER_PORT", "should be the server port"));
+				envp.insert(std::make_pair<std::string, std::string>("REQUEST_METHOD", "should be the request method"));
+				envp.insert(std::make_pair<std::string, std::string>("CONTENT_TYPE", "should be the content type"));
+				envp.insert(std::make_pair<std::string, std::string>("CONTENT_LENGTH", "should be the content length"));
+				envp.insert(std::make_pair<std::string, std::string>("PATH_INFO", "should be the path requested"));
+				envp.insert(std::make_pair<std::string, std::string>("PATH_TRANSLATED", "should be the full path"));
+				envp.insert(std::make_pair<std::string, std::string>("SCRIPT_NAME", "should be the script name"));
+				envp.insert(std::make_pair<std::string, std::string>("QUERY_STRING", "should be the query string"));
+				// and probably some other env vars to setup the sameway .....
+				
+				
 				return this->exec(cgiPath);
 			}
 
